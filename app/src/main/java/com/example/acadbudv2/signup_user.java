053b9dc6ce1c38865fc832f639d6114e9a4432e2
The Misfits <themisfits.acadbud@gmail.com>
2023-10-16 15:04:11 +0800

    db

--- app/src/main/java/com/example/acadbudv2/signup_user.java
+++ app/src/main/java/com/example/acadbudv2/signup_user.java
@@ -1,28 +1,105 @@
 package com.example.acadbudv2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class signup_user extends AppCompatActivity {
+
+    private EditText lrnEditText;
+    private EditText nameEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_user);
 
-        List<users> userList = new database(this).getUsers();
+        lrnEditText = findViewById(R.id.Input_Lrn_signup_user);
+        nameEditText = findViewById(R.id.Input_Name_user);
+        emailEditText = findViewById(R.id.Input_Email_user);
+        passwordEditText = findViewById(R.id.Input_Password_signup_user);
 
-        // Now you can work with the retrieved user data
-        for (users user : userList) {
-            String lrn = user.getLrn();
-            String name = user.getName();
-            String email = user.getEmail();
-            String password = user.getPassword();
+        Button signUpButton = findViewById(R.id.Signupbttn_user);
 
-            // Do something with the user data (e.g., display it in your UI)
-        }
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String lrn = lrnEditText.getText().toString();
+                String name = nameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                // Send the data to your PHP script for registration
+                registerUser(lrn, name, email, password);
+            }
+        });
+    }
+
+    private void registerUser(String lrn, String name, String email, String password) {
+        AsyncTask.execute(() -> {
+            try {
+                OkHttpClient client = new OkHttpClient();
+                MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+                // Define the URL of your PHP script for registration
+                String phpScriptUrl = "http://192.168.1.25/www/register.php"; // Replace with your PHP script URL
+
+                // Create a JSON object with the registration data
+                JSONObject json = new JSONObject();
+                json.put("lrn", lrn);
+                json.put("name", name);
+                json.put("email", email);
+                json.put("password", password);
+
+                RequestBody requestBody = RequestBody.create(JSON, json.toString());
+
+                Request request = new Request.Builder()
+                        .url(phpScriptUrl)
+                        .post(requestBody)
+                        .build();
+
+                Response response = client.newCall(request).execute();
+
+                if (response.isSuccessful()) {
+                    String responseData = response.body().string();
+
+                    // Handle the response from your PHP script
+                    if (responseData.equals("{\"success\": true}")) {
+                        runOnUiThread(() -> {
+                            Intent intent = new Intent(signup_user.this, login_user.class);
+                            startActivity(intent);
+                        });
+                    } else {
+                        runOnUiThread(() -> {
+                            Toast.makeText(this, "Registration failed.", Toast.LENGTH_SHORT).show();
+                        });
+                    }
+                } else {
+                    runOnUiThread(() -> {
+                        Toast.makeText(this, "Registration failed.", Toast.LENGTH_SHORT).show();
+                    });
+                }
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
