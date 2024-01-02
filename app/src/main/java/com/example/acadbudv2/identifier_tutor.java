c954f5e958cf5e34d089763f58f445fd6ae5905f
The Misfits <themisfits.acadbud@gmail.com>
2024-01-02 18:45:46 +0800

    identifier tutor

---
+++ app/src/main/java/com/example/acadbudv2/identifier_tutor.java
@@ -0,0 +1,14 @@
+package com.example.acadbudv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class identifier_tutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.identifier_tutor_dialog);
+    }
+}
