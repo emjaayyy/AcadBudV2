0df9e2f698411022ba91a4170137b606185e6a3b
The Misfits <themisfits.acadbud@gmail.com>
2024-01-10 10:24:37 +0800

    xml at notif adapter

---
+++ app/src/main/java/com/example/acadbudv2/notif_adapter.java
@@ -0,0 +1,14 @@
+package com.example.acadbudv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class notif_adapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notif_adapter);
+    }
+}
